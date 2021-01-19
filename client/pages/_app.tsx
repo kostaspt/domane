fa4b478ef9bea80cce82e66b9cbbcbed1e37fbda
Konstantinos Pittas <konstantinos.pittas@gmail.com>
2021-01-19 09:28:32 +0200

    fix(client): color flashing

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,15 +1,32 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, cookieStorageManager, localStorageManager } from '@chakra-ui/react';
+import App, { AppContext } from 'next/app';
 import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 import store from '../store';
 import theme from '../theme';
 
-export default function CustomApp({ Component, pageProps }: AppProps) {
+type CustomAppProps = AppProps & {
+  cookies: any;
+};
+
+function CustomApp({ Component, cookies, pageProps }: CustomAppProps) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
+    <ChakraProvider
+      resetCSS
+      theme={theme}
+      colorModeManager={typeof cookies === 'string' ? cookieStorageManager(cookies) : localStorageManager}
+    >
       <Provider store={store}>
         <Component {...pageProps} />
       </Provider>
     </ChakraProvider>
   );
 }
+
+CustomApp.getInitialProps = async (appContext: AppContext) => {
+  const appProps = await App.getInitialProps(appContext);
+
+  return { ...appProps, cookies: appContext.ctx.req?.headers.cookie };
+};
+
+export default CustomApp;
