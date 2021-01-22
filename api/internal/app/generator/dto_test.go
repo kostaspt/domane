6ae925a93ee6565a49b9d44629fd09633a52a8f8
Konstantinos Pittas <konstantinos.pittas@gmail.com>
2021-01-22 08:30:18 +0200

    feat(api): add a shorter domain in test

--- api/internal/app/generator/dto_test.go
+++ api/internal/app/generator/dto_test.go
@@ -13,11 +13,13 @@ func TestResultsSort(t *testing.T) {
 		{Domain: "foo.xyz", Kind: CommonExtension},
 		{Domain: "foo.com", Kind: DotComExtension},
 		{Domain: "foo.io", Kind: CommonExtension},
+		{Domain: "f.io", Kind: CommonExtension},
 	}
 
 	sort.Sort(rs)
 
 	assert.Equal(t, "foo.com", rs[0].Domain)
-	assert.Equal(t, "foo.io", rs[1].Domain)
-	assert.Equal(t, "foo.xyz", rs[2].Domain)
+	assert.Equal(t, "f.io", rs[1].Domain)
+	assert.Equal(t, "foo.io", rs[2].Domain)
+	assert.Equal(t, "foo.xyz", rs[3].Domain)
 }
