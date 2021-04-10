0399f4509805ca34459378a743f688e3e8989016
kostaspt <kostaspt@users.noreply.github.com>
2021-04-10 10:51:41 +0000

    fix(client): code format

--- client/src/components/molecules/DomainBuy/index.test.tsx
+++ client/src/components/molecules/DomainBuy/index.test.tsx
@@ -4,17 +4,23 @@ import DomainBuy from './index';
 it('can present an available domain', () => {
   render(<DomainBuy domain="test.com" isAvailable={true} />);
 
-  expect(screen.getByTestId('domain-buy-text')).toHaveTextContent(['🎉','test.com is available! You can buy it via:'].join(''));
+  expect(screen.getByTestId('domain-buy-text')).toHaveTextContent(
+    ['🎉', 'test.com is available! You can buy it via:'].join('')
+  );
 });
 
 it('can present a domain with undefined availability', () => {
   render(<DomainBuy domain="test.com" isAvailable={undefined} />);
 
-  expect(screen.getByTestId('domain-buy-text')).toHaveTextContent(['🤔', 'test.com could be available. Check it using:'].join(''));
+  expect(screen.getByTestId('domain-buy-text')).toHaveTextContent(
+    ['🤔', 'test.com could be available. Check it using:'].join('')
+  );
 });
 
 it('can present a domain with null availability', () => {
   render(<DomainBuy domain="test.com" isAvailable={null} />);
 
-  expect(screen.getByTestId('domain-buy-text')).toHaveTextContent(['🤔', 'test.com could be available. Check it using:'].join(''));
+  expect(screen.getByTestId('domain-buy-text')).toHaveTextContent(
+    ['🤔', 'test.com could be available. Check it using:'].join('')
+  );
 });
